73573e63f166d6eaa9b61b356f36950279e7b12d
sewmini2004 <sewminipremodya98@gmail.com>
2023-04-19 14:34:43 +0530

    Initial Commit

--- src/lk/ijse/hibernate/hostel/controller/LoginFormController.java
+++ src/lk/ijse/hibernate/hostel/controller/LoginFormController.java
@@ -13,18 +13,34 @@ import java.io.IOException;
 public class LoginFormController {
 
     public TextField txt_username;
+//    passwordHidden
     public PasswordField txt_password;
+    //checkBox
     public CheckBox pass_toggle;
     public Label Logins;
+    //passwordText
+    public Label txtPassword;
+
+
 
 
     public void initialize(){
         txt_username.requestFocus();
+
     }
 
 
     public void togglevisiblePassword(ActionEvent event) {
+        if(pass_toggle.isSelected()){
+            txtPassword.setText(txt_password.getText());
+            txtPassword.setVisible(true);
+            txt_password.clear();
+            return;
 
+        }
+        txt_password.setText(txtPassword.getText());
+        txt_password.setVisible(true);
+        txtPassword.setVisible(false);
     }
 
     public void btnLoginOnAction(ActionEvent event) throws IOException {
